44349a6aafe2ff08c3eeeea3c738383a9b95cb74
NyaShinn1204 <team.nyarara@gmail.com>
2025-03-29 12:53:12 +0900

    Remove debug file
    - ミスったからこれ削除

--- test_amazon.py
+++
@@ -1,8 +0,0 @@
-from ext import amazon as amazon_dl
-
-import requests
-
-# 1080の時は SDR
-# 2160の時は HDR
-
-amazon_dl.main_command(requests.Session(), "https://www.amazon.co.jp/gp/video/detail/B0CHHPLWM5/ref=atv_hm_hom_c_lW5Kly_brws_2_1?jic=8%7CEgRzdm9k", "", "", "DEBUG", 1080, "SDR", True)
