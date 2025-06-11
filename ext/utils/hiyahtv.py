32e622abfd445b4eebbae51463d3b0ccf0c3b20b
NyaShinn1204 <team.nyarara@gmail.com>
2025-06-11 22:36:42 +0900

    Done QR_LOGIN
    - idk

--- ext/utils/hiyahtv.py
+++ ext/utils/hiyahtv.py
@@ -13,7 +13,6 @@ class FOD_downloader:
         
         default_headers = {
             "content-type": "application/json",
-            # "host": "id.fod.fujitv.co.jp",
             "connection": "Keep-Alive",
             "accept-encoding": "gzip",
             "user-agent": "Hi-YAH!/8.402.1(Google AOSP TV on x86, Android 16 (API 36))",
@@ -31,42 +30,42 @@ class FOD_downloader:
             """
             # default_headers["host"] = "fod-sp.fujitv.co.jp"
             # self.session.headers.update(default_headers)
-            # get_loginurl = self.session.get("https://fod-sp.fujitv.co.jp/apps/api/login/pin/?dv_type=tv")
-            # if get_loginurl.status_code != 200:
-            #     return False, "Authentication Failed: Failed to get QR login url", None, None, None
-            # else:
-            #     request_login_json = get_loginurl.json()
-            #     print("Login URL:", request_login_json["url"])
-            #     print("Code:", request_login_json["code"])
+            get_loginurl = self.session.get("https://api.vhx.tv/oauth/codes/")
+            if get_loginurl.status_code != 201:
+                return False, "Authentication Failed: Failed to get QR login url", None, None, None
+            else:
+                request_login_json = get_loginurl.json()
+                print("Login URL:", "https://www.hijahtv.com/activate")
+                print("Code:", request_login_json["code"])
                 
-            #     while True:
-            #         send_checkping = self.session.post("https://fod-sp.fujitv.co.jp/apps/api/login/check_pin/", json={"pin": request_login_json["code"]})                        
-            #         if send_checkping.status_code == 400:
-            #             print("Waiting Login...")
-            #             time.sleep(5)
-            #         elif send_checkping.status_code == 200:
-            #             print("Login Accept")
-            #             login_success_json = send_checkping.json()
-            #             gen_token = self.gen_login_uid_token(login_success_json["uid"])
-            #             self.session.headers.update({"x-authorization": "Bearer "+gen_token})
+                start_time = time.time()
+                
+                while True:
+                    if time.time() - start_time >= request_login_json["expires_in"]:
+                        print("Code Expired. Please Re-try")
+                        break
+                    send_checkping = self.session.post(f"https://api.vhx.tv/oauth/codes/{request_login_json["code"]}", params={"client_id": "27ef31d7c3817dfdcb9db4d47fbf9ce92144f361c34fe45e5cd80baab2f258b6","client_secret": "4bc905f4faa17b9e379bbcf0547d7cad710603e316b0a35dc0f3e3568d797bfd"})                        
+                    if send_checkping.status_code == 404:
+                        print("Waiting Login...")
+                        time.sleep(5)
+                    elif send_checkping.status_code == 200:
+                        print("Login Accept")
+                        login_success_json = send_checkping.json()
+                        self.session.headers.update({"x-authorization": "Bearer "+login_success_json["access_token"]})
+                      
+                        status, message = self.get_userinfo()
                         
-            #             status, message, login_uuid = self.get_userinfo()
-            #             fod_user_id = message.get("member_id")
-            #             if message == "1012":
-            #                 return False, "Authentication Failed: This account is not subscription", None, None, None
-            #             else:
-            #                 self.logined_headers = self.session.headers
-            #                 self.login_status = [False, True]
-                            
-            #                 session_json = {
-            #                     "method": "QR_LOGIN",
-            #                     "email": hashlib.sha256(email.encode()).hexdigest(),
-            #                     "password": hashlib.sha256(password.encode()).hexdigest(),
-            #                     "access_token": gen_token,
-            #                     "refresh_token": None
-            #                 }
-                            
-            #                 return True, message, login_uuid, self.login_status, session_json
+                        session_json = {
+                            "method": "QR_LOGIN",
+                            "email": hashlib.sha256(email.encode()).hexdigest(),
+                            "password": hashlib.sha256(password.encode()).hexdigest(),
+                            "access_token": login_success_json["access_token"],
+                            "refresh_token": login_success_json["refresh_token"]
+                        }
+                        
+                        self.login_status = True
+                        
+                        return True, message, self.login_status, session_json
         
         if not re.fullmatch('[0-9]+', email):
             if not re.fullmatch(mail_regex, email):
@@ -140,30 +139,12 @@ class FOD_downloader:
             return True, temp_token.json()
         else:
             return False, None
-    def gen_login_uid_token(self, uid):
-        secret_key = "II1pq1aFylVZNASr0mea7zXFOhrAPZURZp6Ru3LuqqsUVZ4lyJj2R4kufetQN9mx" # Haha cracked from AndroidTV APK
-        device_type = "androidTV"
-        device_id = "google_google_aosp tv on x86_13"
-        
-        payload = {
-            "iss": "FOD",
-            "uid": uid,
-            "dv_type": device_type,
-            "dv_id": device_id,
-        }
-        
-        jwt_token = jwt.encode(payload, secret_key, algorithm='HS256')
-        return jwt_token
     
     def get_userinfo(self):
-        global fod_user_id
-        url = "https://fod-sp.fujitv.co.jp/apps/api/user/status/"
-        
-        querystring = { "dv_type": "tv" }
-            
-        response = self.session.get(url, params=querystring)
+        url = "https://api.vhx.com/v2/sites/90901/me"
+                    
+        response = self.session.get(url)
         if response.status_code == 200:
-            fod_user_id = response.json().get("member_id")
-            return True, response.json(), response.cookies.get("uuid")
-        elif response.status_code == 401:
-            return False, response.json["code"], None
+            return True, response.json()
+        elif response.status_code == 400:
+            return False, None
