5350ea80515ebc4cb68957183360f63232deeff1
NyaShinn1204 <team.nyarara@gmail.com>
2025-06-12 18:05:19 +0900

    Add normal login
    - Hehe. client id leaked 💀

--- ext/utils/hiyahtv.py
+++ ext/utils/hiyahtv.py
@@ -71,61 +71,28 @@ class HI_YAH_downloader:
             if not re.fullmatch(mail_regex, email):
                 return False, "Hi-YAH! require email and password", None, None, None
             
-        # payload = {
-        #     "mail_address": email,
-        #     "password": password
-        # }    
+        payload = {
+            "client_id": "27ef31d7c3817dfdcb9db4d47fbf9ce92144f361c34fe45e5cd80baab2f258b6",      # From Android TV
+            "client_secret": "4bc905f4faa17b9e379bbcf0547d7cad710603e316b0a35dc0f3e3568d797bfd",  # From Android TV
+            "username": "adobepd50@gmail.com",
+            "grant_type": "password",
+            "password": "Mohsen1234"
+        }
         
-        # response = self.session.post("https://id.fod.fujitv.co.jp/api/member/v2/login_app", headers=default_headers, json=payload)
-        # response.raise_for_status()
+        response = self.session.post("https://auth.vhx.com/v1/oauth/token", headers=default_headers, json=payload)
+        response.raise_for_status()
         
-        # email_verify_hashkey = response.json()["hash_key"]
-        # mail_auth_code = input("MAIL AUTH CODE : ")
-        # if mail_auth_code == None:
-        #     return False, "Authentication Failed: Require Mail Auth Code", None, None, None
-        # else:
-        #     pass
-        
-        # payload = {
-        #     "auth_code": str(mail_auth_code),
-        #     "hash_key": email_verify_hashkey
-        # }
-        # login_status_check = self.session.post("https://id.fod.fujitv.co.jp/api/member/CheckAuthCodeApp", headers=default_headers, json=payload)
-        # login_status_check.raise_for_status()
-        
-        # fodid_login_token = login_status_check.json()["fodid_login_token"]
-        
-        # # default_headers["host"] = "fod-sp.fujitv.co.jp"
-        # self.session.headers.update(default_headers)
-        
-        # payload = {
-        #     "fodid_login_token": fodid_login_token
-        # }
-        # check_token_status = self.session.post("https://fod-sp.fujitv.co.jp/apps/api/login/check_token/", headers=default_headers, json=payload)
-        # check_token_status.raise_for_status()
-        
-        # uid = check_token_status.json()["uid"]
-        
-        # login_token = self.gen_login_uid_token(uid)
-        
-        # self.session.headers.update({"x-authorization": "Bearer "+login_token})
-        
-        # status, message, login_uuid = self.get_userinfo()
-        # fod_user_id = message.get("member_id")
-        # if message == "1012":
-        #     return False, "Authentication Failed: This account is not subscription", None, None, None
-        # else:
-        #     self.logined_headers = self.session.headers
-        #     self.login_status = [False, True]
-        #     session_json = {
-        #         "method": "NORMAL",
-        #         "email": hashlib.sha256(email.encode()).hexdigest(),
-        #         "password": hashlib.sha256(password.encode()).hexdigest(),
-        #         "access_token": login_token,
-        #         "refresh_token": None
-        #     }
-        #     return True, message, login_uuid, self.login_status, session_json
-
+        status, message = self.get_userinfo()
+        self.login_status = True
+        session_json = {
+            "method": "NORMAL",
+            "email": hashlib.sha256(email.encode()).hexdigest(),
+            "password": hashlib.sha256(password.encode()).hexdigest(),
+            "access_token": response.json()["acccess_token"],
+            "refresh_token": response.json()["refresh_token"]
+        }
+        return True, message, self.login_status, session_json
+    
     def get_temp_token(self):
         payload = {
           "client_id": "27ef31d7c3817dfdcb9db4d47fbf9ce92144f361c34fe45e5cd80baab2f258b6",      # From Android TV
@@ -164,3 +131,15 @@ class HI_YAH_downloader:
             return True, refresh_return.json()
         else:
             return False, None
+        
+    def revoke_token(self, token):
+        payload = {
+          "client_id": "27ef31d7c3817dfdcb9db4d47fbf9ce92144f361c34fe45e5cd80baab2f258b6",      # From Android TV
+          "client_secret": "4bc905f4faa17b9e379bbcf0547d7cad710603e316b0a35dc0f3e3568d797bfd",  # From Android TV
+          "token": token,
+        }
+        revoke_status = self.session.post("https://api.vhx.tv/oauth/revoke", json=payload)
+        if revoke_status.status_code == 200:
+            return True
+        else:
+            return False
