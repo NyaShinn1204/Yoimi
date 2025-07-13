1a41ea99e19c617397d7ef5834e44fe58aed5d37
NyaShinn1204 <team.nyarara@gmail.com>
2025-07-13 22:23:36 +0900

    Sample here
    - on going

---
+++ ext/services/h_next.py
@@ -0,0 +1,11 @@
+def main_command(url, command_list):
+    # sample service
+    
+    ## SERVICE INFO
+    ## name: H-Next
+    ## cache_session: Yes
+    ## support_url: 
+    ##    https://video.hnext.jp/title/xxx
+    ##    https://video.hnext.jp/play/xxx/xxx
+    
+    pass
