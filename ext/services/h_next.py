fe80c2cfca6e7ab0d7436515fff350048f4aeb6f
NyaShinn1204 <team.nyarara@gmail.com>
2025-07-14 22:28:53 +0900

    Rewrite Logging system
    - hehe

--- ext/services/h_next.py
+++ ext/services/h_next.py
@@ -1,11 +1,20 @@
-def main_command(url, command_list):
-    # sample service
-    
+from ext.util import Logger
+
+__service_name__ = "H-Next"
+__user_agent__ = "Mozilla/5.0 (Linux; Android 10; A7S Build/QP1A.190711.020; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/81.0.4044.138 Mobile Safari/537.36 japanview/1.0.6"
+
+def main_command(email, password, url, command_list):    
     ## SERVICE INFO
     ## name: H-Next
+    ## require_account: Yes
     ## cache_session: Yes
     ## support_url: 
     ##    https://video.hnext.jp/title/xxx
     ##    https://video.hnext.jp/play/xxx/xxx
+    
+    logger = Logger.create_logger(service_name=__service_name__, LOG_LEVEL=command_list["verbose"])
+    
+    if not email or not password:
+        logger.error(f"{__service_name__} is require account login.")
     
     pass
--- ext/util.py
+++ ext/util.py
@@ -1,5 +1,7 @@
 import re
 import os
+import time
+import logging
 
 from typing import Iterator
 
@@ -16,6 +18,54 @@ def path_check(input_path):
     looks_like_path = '/' in input_path or '\\' in input_path
 
     return has_extension or looks_like_path
+
+class Logger:
+    COLOR_GREEN = "\033[92m"
+    COLOR_GRAY = "\033[90m"
+    COLOR_RESET = "\033[0m"
+    COLOR_BLUE = "\033[94m"
+    class CustomFormatter(logging.Formatter):
+        def format(self, record):
+            service_name = getattr(record, "service_name", "")
+            levelname = record.levelname
+            record.color_service_name = f"{Logger.COLOR_BLUE}{service_name}{Logger.COLOR_RESET}" if service_name else ""
+            record.color_levelname = f"{Logger.COLOR_GRAY}{levelname}{Logger.COLOR_RESET}"
+            
+            if not hasattr(record, "asctime"):
+                record.asctime = self.formatTime(record, self.datefmt)
+            record.color_asctime = f"{Logger.COLOR_GREEN}{record.asctime}{Logger.COLOR_RESET}"
+    
+            self._style._fmt = (
+                "%(color_asctime)s [%(color_levelname)s] %(color_service_name)s : %(message)s"
+            )
+            return super().format(record)
+    
+    class ServiceLoggerAdapter(logging.LoggerAdapter):
+        def process(self, msg, kwargs):
+            kwargs.setdefault('extra', {})
+            kwargs['extra'].setdefault('service_name', self.extra['service_name'])
+            return msg, kwargs
+    
+    def create_logger(service_name: str, LOG_LEVEL: bool):
+        base_logger = logging.getLogger('YoimiLogger')
+        if LOG_LEVEL == "DEBUG":
+            base_logger.setLevel(logging.DEBUG)
+        else:
+            base_logger.setLevel(logging.INFO)
+    
+        formatter = Logger.CustomFormatter(
+            '%(asctime)s [%(levelname)s] %(service_name)s : %(message)s',
+            datefmt='%Y-%m-%d %H:%M:%S'
+        )
+    
+        console_handler = logging.StreamHandler()
+        console_handler.setFormatter(formatter)
+    
+        if not base_logger.handlers:
+            base_logger.addHandler(console_handler)
+        
+        logger = Logger.ServiceLoggerAdapter(base_logger, {'service_name': service_name})
+        return logger
 
 def download_command(input: str, command_list: Iterator):
     pass
